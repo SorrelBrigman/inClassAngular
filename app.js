5f7d06119ec475f2c45fcafebd5ddf175e45e03e
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-17 15:22:11 -0600

    class push after  exercise

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const app = angular.module("monkeyButtApp", ['ngRoute'])
 
 
-app.config(($routeProvider)=>{
+app.config(($routeProvider, $locationProvider)=>{
+  $locationProvider.hashPrefix('');
+
   $routeProvider
     .when("/", {
       controller: "MainCtrl",
@@ -12,21 +14,50 @@ app.config(($routeProvider)=>{
       controller: "ListCtrl",
       templateUrl: "partials/list.html"
     })
+    .when("/list/:somevariable", {
+      controller: "DetailCtrl",
+      templateUrl: "partials/details.html"
+
+    })
+    .otherwise( {
+      redirectTo: "/"
+    })
 })
 
-app.controller("MainCtrl", function($scope) {
+app.controller("MainCtrl", function($scope,) {
   $scope.potatoes = "bake!"
+  $scope.onClickFunction = (arg)=>{
+    console.log("my function on scope")
+    console.log("arg: ", arg)
+  }
 })
 
 
 
 app.controller("ListCtrl", function($scope, $http){
-  console.log("Here in the list controller")
+  console.log("Here is the list controller")
 
   $http.get("list.json")
   .then((val)=> {
     console.log("list.json", val.data);
     $scope.list = val.data.list;
+  })
+  .catch(()=>{
+    console.log("rejected!")
+  })
+})
+
+
+app.controller("DetailCtrl", function($scope, $routeParams, $http){
+  console.log("Here is the detail controller")
+
+  $scope.thatNumber = $routeParams.somevariable;
+
+  $http.get("list.json")
+  .then((val)=> {
+    console.log("list.json", val.data);
+    let list = val.data.list
+    $scope.selectedItem = list[$scope.thatNumber];
   })
   .catch(()=>{
     console.log("rejected!")
