c5d2dab74c08cbb313d2614f5ff32bebe48410f0
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-18 12:08:15 -0600

    before lunch Wednesday

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,7 +1,16 @@
-app.controller("MainCtrl", function($scope,) {
+app.controller("MainCtrl", function($scope, varFactory) {
   $scope.potatoes = "bake!"
   $scope.onClickFunction = (arg)=>{
     console.log("my function on scope")
     console.log("arg: ", arg)
   }
+
+  $scope.thingFromFactory = varFactory.getThatVar();
+
+  $scope.setter = (newVal) => {
+    console.log("newVal", newVal)
+    varFactory.setThatVar(newVal)
+    $scope.thingFromFactory = newVal;
+  }
+
 })
---
+++ app/factories/varFactory.js
@@ -0,0 +1,17 @@
+app.factory("varFactory", function() {
+
+let bologna;
+
+  return {
+    //getter
+    getThatVar : ()=>{
+      return "that var!"
+    },
+    //setter
+    setThatVar: () =>{
+      bologna
+    }
+  }
+
+
+})
